231e4ec69512d2c35bf64d8df03bcaad3521d7f0
Ishrz <ishrzz66@gmail.com>
2025-12-29 19:43:25 +0530

    styling the result card

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -2,22 +2,24 @@
 const ResultCard = ({elem}) => {
     // console.log(elem.src)
   return (
-    <div className="w-[20vw] overflow-hidden  shrink-0 bg-gray-600">
-      <div className="img-container w-full rounded-full">
+    <div className="w-[18vw] h-80 relative bg-gray-600">
+      <div className="card-container h-full w-full rounded-full ">
         {elem.type=='photo' ? <img 
-        className="w-full object-cover h-full  " 
+        className="w-full object-cover h-full  object-center " 
         src={elem.src}
          />: ''}
          {elem.type=='Video'? <video autoPlay muted 
-        className="w-full object-cover h-full  " 
+        className="w-full object-cover h-full object-center  " 
         src={elem.src}
          ></video> :''}
          {elem.type=='Gif' ? <img 
-        className="w-full object-cover h-full  " 
+        className="w-full object-cover h-full object-center line-clamp-2 " 
         src={elem.src}
          alt={elem.tittle}
          />: ''}
-        
+      </div>
+      <div id="bottom-title" className="text-neutral-300 absolute bottom-0 line-clamp-2  w-full h-[20%] px-2 py-2">
+        <h1 className="font-bold capitalize">{elem.tittle}</h1>
       </div>
     </div>
   );
