24b6f46e2ac343cdec0e65e759b01c477887fea1
Ishrz <ishrzz66@gmail.com>
2025-12-31 19:14:14 +0530

    name change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,39 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between w-full p-5 bg-neutral-800">
       <div className="">
-        <Link to={'/'}>
-        <div className='flex items-center '>
-          <div className='w-fit flex items-center '>
-        <img className='w-15 mt-2 object-cover' src={'../logo-removebg.png'}/>
+        <Link to={"/"}>
+          <div className="flex items-center  ">
+            <div className="w-fit flex items-center rounded-full overflow-hidden ">
+              <img
+                className="w-15 mt-2 object-cover"
+                src={"../logo-removebg.png"}
+              />
+            </div>
+            <h1
+              id="logoHeading"
+              className=" font-bold italic  text-3xl text-shadow-red-500 text-cyan-400 shadow-2xl"
+            >
+              MediaStore
+            </h1>
+          </div>
+        </Link>
       </div>
-          <h1 className=" font-bold italic  text-3xl text-shadow-red-500 text-cyan-400 shadow-2xl">
-        MediaStore
-      </h1>
-        </div>
-      
-      </Link>
-      </div>
-      <div className='flex items-center justify-between gap-3'>
-        <Link className='px-3 py-2.5 bg-blue-400 rounded-2xl text-xl font-medium hover:bg-blue-700 active:scale-95 transition ' to={'/'}>Search</Link>
-        <Link className='px-3 py-2.5 bg-blue-400 rounded-2xl text-xl font-medium hover:bg-blue-700 active:scale-95 transition' to={'/collection'}>Collection's</Link>
+      <div className="flex items-center justify-between gap-3">
+        <Link
+          className="px-3 py-2.5 bg-blue-400 rounded-2xl text-xl font-medium hover:bg-blue-700 active:scale-95 transition "
+          to={"/"}
+        >
+          Search
+        </Link>
+        <Link
+          className="px-3 py-2.5 bg-blue-400 rounded-2xl text-xl font-medium hover:bg-blue-700 active:scale-95 transition"
+          to={"/collection"}
+        >
+          Collection's
+        </Link>
       </div>
     </div>
   );
